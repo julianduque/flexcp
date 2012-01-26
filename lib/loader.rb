1d115231987bb027067429cc5469198fd7c31f44
Julian Duque <julianduquej@gmail.com>
2012-01-25 22:51:39 -0500

    Base CLI for FlexCP

---
+++ lib/loader.rb
@@ -0,0 +1,17 @@
+require 'mongo'
+
+module FlexCP
+  class Loader
+
+    def initialize
+    end
+    
+    def help
+      puts 'Usage: flexcp load'
+      puts ''
+    end
+    
+    def load(file)
+    end
+  end
+end
---
+++ lib/processor.rb
@@ -0,0 +1,16 @@
+module FlexCP
+  class Processor
+    
+    def initialize
+    end
+  
+    def help
+      puts 'Usage: flexcp process'
+      puts ''
+    end
+    
+    def process
+    end
+    
+  end
+end
