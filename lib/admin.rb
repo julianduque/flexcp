786c28e858d6e7e2361d637fdfbf7c4ff4e7178f
Julian Duque <julianduquej@gmail.com>
2012-03-13 23:06:52 -0500

    Administration features implemented and file loading in MongoDB

--- lib/admin.rb
+++ lib/admin.rb
@@ -2,6 +2,8 @@ module FlexCP
   class Admin
 
     def initialize
+      db = Mongo::Connection.new.db("flexcp")
+      @projects = db['projects']
     end
     
     def help(command)
@@ -16,15 +18,38 @@ module FlexCP
     end
 
     def list
-      puts 'list all projects'
-    end
-
-    def new(project)
-      puts "new project"      
+      if @projects.count() > 0
+        puts "Listing projects"
+        @projects.find().each do |project|
+          puts "*  #{project['name']}"
+        end
+      else
+        puts "No projects found"
+      end
     end
     
-    def delete(project)
-      puts "delete project"
+    def show(project_name)
+    end
+
+    def new(project_name)
+      project = @projects.find("name" => project_name).first
+      if project
+        puts "project '#{project_name}' already exists"
+      else
+        project = { "name" => project_name }
+        @projects.insert(project)
+        puts "project '#{project_name}' successfully created"
+      end
+    end
+    
+    def delete(project_name)
+      project = @projects.find("name" => project_name).first
+      if project
+        @projects.remove("name" => project_name)
+        puts "project '#{project_name}' successfully removed"
+      else
+        puts "project '#{project_name}' doesn't exist"
+      end
     end
     
   end
--- lib/loader.rb
+++ lib/loader.rb
@@ -1,18 +1,47 @@
-require 'mongo'
-
 module FlexCP
   class Loader
 
     def initialize
+      db = Mongo::Connection.new.db('flexcp')
+      @projects = db['projects']
+      @trajectories = db['trajectories'] 
     end
     
     def help
-      puts 'Usage: flexcp load'
-      puts ''
+      puts 'Usage: flexcp load <project> <height> <file>'
     end
     
-    def load(filename)
-      puts filename
+    def load(project_name, height, filename)
+      project = @projects.find('name' => project_name).first
+      if project
+        @trajectories.remove("_height" => height)
+        file = File.new(filename, 'r')
+        file.each_line do |row|
+          if file.lineno > 1
+            columns = row.split(',')
+            content = {
+              "_project_id" => project["_id"],
+              "_height" => height,
+              "start_year" => columns.shift.strip,
+              "start_month" => columns.shift.strip,
+              "start_date" => columns.shift.strip,
+              "start_hour" => columns.shift.strip,
+              "year" => columns.shift.strip,
+              "month" => columns.shift.strip,
+              "day" => columns.shift.strip,
+              "hour" => columns.shift.strip,
+              "age_hour" => columns.shift.strip,
+              "latitude" => columns.shift.strip,
+              "longitude" => columns.shift.strip,
+              "height" => columns.shift.strip,
+              "press" => columns.shift.strip
+            }
+            @trajectories.insert(content) 
+          end
+        end
+      else
+        puts "project '#{project_name}' not found"
+      end
     end
   end
 end
