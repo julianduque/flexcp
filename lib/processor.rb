9621547900b13bc7a85422166185bfd64cd48381
Julian Duque <julianduquej@gmail.com>
2012-05-08 22:58:25 -0500

    Process the file

--- lib/processor.rb
+++ lib/processor.rb
@@ -24,12 +24,18 @@ module FlexCP
               Dir::mkdir("out/#{project_name}") unless File.exists?("out/#{project_name}")
               Dir::mkdir("out/#{project_name}/#{t}") unless File.exists?("out/#{project_name}/#{t}")
               
-              file = File.open("out/#{project_name}/#{t}/n#{row['start_year']}#{row['start_month']}#{row['start_date']}#{row['start_hour']}", 'w')
+              file = File.open("out/#{project_name}/#{t}/n20#{row['start_year']}#{row['start_month']}#{row['start_date']}#{row['start_hour']}", 'w')
+              file.puts "     1"
+              file.puts "    FNL     #{row['start_year']}    #{row['start_month']}    #{row['start_date']}     0     0"
+              file.puts "     1BACKWARDOMEGA"
+              file.puts "    #{row['start_year']}    #{row['start_month']}     #{row['start_date']}    #{row['start_hour']}" + align_value("#{row['latitude']}", 12)+ align_value("#{row['longitude']}",12) + align_value("#{row['height']}",10)
+              file.puts "     1PRESSURE"
             end
+            
             if file
-              file.puts "Hello"
-            end            
-          end          
+              file.puts "     1     1   #{row['start_year']}    #{row['start_month']}     #{row['start_date']}    #{row['start_hour']}     0     0  " + align_value("#{row['age_hour']}",6) + align_value("#{row['latitude']}", 12)+ align_value("#{row['longitude']}",12) + align_value("#{row['height']}",10) + align_value("#{row['press']}", 10)
+            end
+          end
         end
       else
         puts "project '#{project_name}' not found"
@@ -40,6 +46,11 @@ module FlexCP
     
     def get_project(project_name)
       @projects.find("name" => project_name).first
+    end
+    
+    def align_value(value, length)
+      spaces = length - value.length
+      ' ' * spaces + value
     end
     
   end
